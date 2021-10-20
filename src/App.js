bc00054e24a9c79ff2e0be1c082ccb4f4c62d7d5
Sandeep Rajakrishnan <sandur43@gmail.com>
2021-10-20 23:11:41 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Products from './components/Products/Products';
+function App() {
+  return (
+    <div className="App">
+ <Navbar></Navbar>
+ <Products></Products>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {Card, CardMedia, CardActions, CardContent, Typography, IconButton} from '@material-ui/core'
+import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from './styles'
+
+const Product = ({product}) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.image} title={product.name}/>
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+
+                </div>
+                <Typography variant="body2" color="textSecondary">
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to cart">
+                    <AddShoppingCart/>
+                </IconButton>
+            </CardActions>
+
+        </Card>
+
+    )
+}
+
+export default Product;
