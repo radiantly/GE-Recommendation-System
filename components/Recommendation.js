63a21f7927d90e8b9892dc550b9ab7ac1f5df14e
samkeating <sanayukthavpksr@gmail.com>
2021-11-09 16:12:34 +0530

    Changed Recomm

--- components/Recommendation.js
+++ components/Recommendation.js
@@ -1,9 +1,18 @@
 import styles from "../styles/Recomendation.module.css";
-
 function Recommendation() {
     return (
         <div className={styles.recomendation__main}>
-        <div className={styles.recomendation__header}><h1>Get recommendation</h1></div>   
+        <div className={styles.recomendation__header}><h1>Get recommendation</h1></div>
+        <div className={styles.recommendation__header2}>See recommendations for&nbsp; <div className={styles.recommendation__link}><u>Lucy Ann</u></div></div>
+        <div className={styles.recommendation__squarer1}>
+        <div className={styles.recommendation__square}></div>
+        <div className={styles.recommendation__square}></div>  
+        <div className={styles.recommendation__square}></div> 
+        <div className={styles.recommendation__square}></div>
+        <div className={styles.recommendation__square}></div>
+        <div className={styles.recommendation__square}></div> 
+        <div className={styles.recommendation__square}></div>
+        <div className={styles.recommendation__square}></div></div>   
         </div>
     )
 }
