8be73f449da0b8241da057e263b2a7a32cdf0376
samkeating <sanayukthavpksr@gmail.com>
2021-11-11 23:33:19 +0530

    Implemented dropdown

--- components/Navbar.js
+++ components/Navbar.js
@@ -24,11 +24,17 @@ const Navbar = () => {
         <input placeholder="Search for products..." />
       </label>
       <div className={styles.spacer}></div>
-      <Link href="/">
-        <div className={styles.box}>
-          <a className={styles.text}>Login/Register</a>
-        </div>
-      </Link>
+      <div className={styles.dropdown}>
+        <a className={styles.text}>Login/Register</a>
+                    <div className={styles.dropdown_content}>
+                        <a href="#">User 1</a>
+                        <a href="#">User 2</a>
+                        <a href="#">User 3</a>
+                        <a href="#">User 4</a>
+                        <a href="#">User 5</a>
+                        <a href="#">User 6</a>
+                    </div>
+                </div> 
     </nav>
   );
 };
--- components/Recommendation.js
+++ components/Recommendation.js
@@ -3,7 +3,19 @@ function Recommendation() {
     return (
         <div className={styles.recomendation__main}>
         <div className={styles.recomendation__header}><h1>Get recommendation</h1></div>
-        <div className={styles.recommendation__header2}>See recommendations for&nbsp; <div className={styles.recommendation__link}><u>Lucy Ann</u></div></div>
+        <div className={styles.recommendation__header2}>See recommendations for&nbsp;
+        <div className={styles.dropdown}>
+        <a className={styles.recommendation__link1}><u>Lucy Ann</u></a>
+                    <div className={styles.dropdown_content}>
+                        <a href="#">User 1</a>
+                        <a href="#">User 2</a>
+                        <a href="#">User 3</a>
+                        <a href="#">User 4</a>
+                        <a href="#">User 5</a>
+                        <a href="#">User 6</a>
+                    </div>
+                </div> 
+                </div>
         <div className={styles.recommendation__squarer1}>
         <div className={styles.recommendation__square}></div>
         <div className={styles.recommendation__square}></div>  
