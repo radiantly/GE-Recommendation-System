fb47900f1498d267f074aa6e163ce6a3ff1b168c
radiantly <buildingsomethingfun@gmail.com>
2021-12-04 13:47:38 +0530

    Add CORS to lambda function

--- Misc/getRecLambda.py
+++ Misc/getRecLambda.py
@@ -1,39 +1,36 @@
-import boto3
 import json
 
+import boto3
+
+
 def lambda_handler(event, context):
-    
+
     query = event["queryStringParameters"]
 
-    print(f"Query parameters: {query}")
-
     if not query:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({ 'message': 'No query parameters found' })
-        }
+        return {"statusCode": 400, "body": json.dumps({"message": "No query parameters found"})}
 
     user_id = query["userID"]
 
     if not user_id:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({ 'message': 'Please specify a user ID' })
-        }
+        return {"statusCode": 400, "body": json.dumps({"message": "Please specify a user ID"})}
 
-    print(f"User ID: {user_id}")
-
-    personalize_runtime = boto3.client('personalize-runtime')
+    personalize_runtime = boto3.client("personalize-runtime")
     campaign_arn = ""
 
     get_recommendations_response = personalize_runtime.get_recommendations(
-        campaignArn = campaign_arn,
-        userId = str(user_id),
+        campaignArn=campaign_arn,
+        userId=str(user_id),
     )
 
-    item_list = get_recommendations_response['itemList']
-    
+    item_list = get_recommendations_response["itemList"]
+
     return {
-        'statusCode': 200,
-        'body': json.dumps({ 'items': item_list })
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+        },
+        "body": json.dumps({"items": item_list}),
     }
