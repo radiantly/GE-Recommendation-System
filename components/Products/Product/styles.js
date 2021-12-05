788273fc383aa0c81cc86ec597f1c749e75e66da
Vighnesh Shankar <68947762+VighneshShankar23@users.noreply.github.com>
2021-12-05 11:36:05 +0530

    CSS changes

--- components/Products/Product/styles.js
+++ components/Products/Product/styles.js
@@ -1,14 +1,15 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+var BackgroundColor = "RED"
+
 export default makeStyles(() => ({
   root: {
     // maxWidth: 345, original width style
     maxWidth: '100%',
-    marginTop:'20%',
+    marginTop: '20%',
   },
   media: {
     height: 200,
-    
     paddingTop: '56.25%', // 16:9
   },
   cardActions: {
@@ -16,7 +17,7 @@ export default makeStyles(() => ({
     justifyContent: 'flex-end',
   },
   cardContent: {
-   // display: 'flex',
+    // display: 'flex',
     justifyContent: 'space-between',
   },
 }));
