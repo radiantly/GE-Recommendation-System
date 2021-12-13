fe54c2e986b4e8d9a13043547b2c805a34e3d731
radiantly <buildingsomethingfun@gmail.com>
2021-12-13 23:41:28 +0530

    Fix iframe height

--- pages/store/[itemId].js
+++ pages/store/[itemId].js
@@ -1,14 +1,28 @@
 import styles from "../../styles/Store.module.css";
+import { useState } from "react";
 
 import * as items from "../../Misc/items.json";
 
 export default function Store({ ITEM_PRODUCT_LINK }) {
+  const [iFrameHeight, setIframeHeight] = useState("100vh");
+
+  const loadHandler = (e) => {
+    setIframeHeight(
+      e.target.contentWindow.document.documentElement.scrollHeight + "px"
+    );
+  };
   const storeURL = ITEM_PRODUCT_LINK.replace(/^https?:\/\/[^/]+/, "");
   return (
     <>
       <div className={styles.iFrameWrap}>
         <div className={styles.loader}></div>
-        <iframe className={styles.storeFrame} src={storeURL} />
+        <iframe
+          style={{ height: iFrameHeight }}
+          className={styles.storeFrame}
+          src={storeURL}
+          onLoad={loadHandler}
+          scrolling="no"
+        />
       </div>
     </>
   );
