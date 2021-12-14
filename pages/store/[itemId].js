92fc7b1012ecb743f1aa0c181636467c333598c0
radiantly <buildingsomethingfun@gmail.com>
2021-12-15 01:53:07 +0530

    Add carousel for similar items recipe

--- pages/store/[itemId].js
+++ pages/store/[itemId].js
@@ -1,17 +1,42 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import styles from "../../styles/Store.module.css";
-import { useState } from "react";
 
 import * as items from "../../Misc/items.json";
+import * as similar_items from "../../Misc/similar_items.json";
 
-export default function Store({ ITEM_PRODUCT_LINK }) {
+export default function Store({ ITEM_ID, ITEM_PRODUCT_LINK }) {
   const [iFrameHeight, setIframeHeight] = useState("100vh");
+  const [simVisible, setSimVisible] = useState(false);
+
+  useEffect(() => setTimeout(setSimVisible, 2000, true), []);
 
   const loadHandler = (e) => {
     setIframeHeight(
       e.target.contentWindow.document.documentElement.scrollHeight + "px"
     );
+    setSimVisible(true);
   };
   const storeURL = ITEM_PRODUCT_LINK.replace(/^https?:\/\/[^/]+/, "");
+
+  const arrowGen = (arrow, { onClick }) => {
+    return (
+      <div
+        className={[styles[arrow], styles.arrow].join(" ")}
+        onClick={onClick}
+      >
+        <span className="material-icons-round">{`arrow_${arrow}_ios`}</span>
+      </div>
+    );
+  };
+
+  const PrevArrow = arrowGen.bind(null, "back");
+  const NextArrow = arrowGen.bind(null, "forward");
+
   return (
     <>
       <div className={styles.iFrameWrap}>
@@ -23,13 +48,45 @@ export default function Store({ ITEM_PRODUCT_LINK }) {
           onLoad={loadHandler}
           scrolling="no"
         />
+        <div
+          className={[
+            styles.simItemsWrap,
+            simVisible ? "" : styles.hidden,
+          ].join(" ")}
+        >
+          <h3>Similar items</h3>
+          <Slider
+            dots={false}
+            infinite={true}
+            slidesToShow={5}
+            prevArrow={<PrevArrow />}
+            nextArrow={<NextArrow />}
+          >
+            {similar_items[ITEM_ID].map((ITEM_ID) => (
+              <div className={styles.sim_square} key={ITEM_ID}>
+                <Link href={`/store/${ITEM_ID}`}>
+                  <a>
+                    <Image
+                      src={items[ITEM_ID]["ITEM_IMAGE_LINK"]}
+                      alt={items[ITEM_ID]["ITEM_NAME"]}
+                      height="1000"
+                      width="1000"
+                    />
+                    <div className={styles.itemTitle}>
+                      {items[ITEM_ID]["ITEM_NAME"]}
+                    </div>
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </Slider>
+        </div>
       </div>
     </>
   );
 }
 
 export async function getStaticProps({ params: { itemId } }) {
-  console.log(itemId);
   if (!(itemId in items))
     return {
       notFound: true,
@@ -37,7 +94,7 @@ export async function getStaticProps({ params: { itemId } }) {
 
   return {
     props: {
-      ID: itemId,
+      ITEM_ID: itemId,
       ...items[itemId],
     },
   };
